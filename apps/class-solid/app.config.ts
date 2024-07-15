e44283ad112a2c7bc91cc5271d063cfd0411e6ab
Peter Kalverla <peter.kalverla@gmx.com>
2024-07-15 11:11:17 +0200

    pnpm create solid + set to SSG

---
+++ apps/class-solid/app.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "@solidjs/start/config";
+
+export default defineConfig({
+  server: {
+    static: true,
+    prerender: {
+      routes: [],
+      crawlLinks: true,
+    },
+  },
+});
