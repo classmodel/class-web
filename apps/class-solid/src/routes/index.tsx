87b50aa3ecd606dd31e5ee136b8f53fbf445caa9
Peter Kalverla <peter.kalverla@gmx.com>
2024-07-15 11:48:11 +0200

    start modifying template

--- apps/class-solid/src/routes/index.tsx
+++ apps/class-solid/src/routes/index.tsx
@@ -1,24 +1,20 @@
-import { A } from "@solidjs/router";
-import Counter from "~/components/Counter";
+import { createSignal } from "solid-js";
+import { Button } from "~/components/ui/button";
+import { createStore } from "solid-js/store"
 
 export default function Home() {
+  const [experiments, setExperiments] = createStore({
+    experiments: [],
+    activeExperiment: -1
+  })
+
+  const [ edit, setEdit] = createSignal(false)
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Hello world!</h1>
-      <Counter />
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Welcome to CLASS</h1>
+      <Button variant='outline' size='lg' onClick={()=>setEdit(true)}>Add experiment</Button>
       <p class="mt-8">
-        Visit{" "}
-        <a href="https://solidjs.com" target="_blank" class="text-sky-600 hover:underline">
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
+        Start your first experiment by clicking this beautiful button
       </p>
     </main>
   );
