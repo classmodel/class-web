57726b5efe9bb39a742254a69fd2ca0f2873bedc
Peter Kalverla <peter.kalverla@gmx.com>
2024-07-16 13:34:54 +0200

    Add group descriptions

---
+++ packages/class/src/runclass.ts
@@ -0,0 +1,18 @@
+import { CLASS } from "./class";
+import { classConfig } from "./config";
+import { ClassOutput } from "./runner";
+
+const config = classConfig.parse({});
+const model = new CLASS(config);
+const output: ClassOutput = { t: [], h: [] };
+
+while (model.t < config.timeControl.runtime) {
+  model.update();
+
+  if (model.t % 60 == 0) {
+    output.t.push(model.t);
+    output.h.push(model.h);
+  }
+}
+
+console.log(output);
