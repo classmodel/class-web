d3eab832287762bb7d82f58ce9ffc471225b82ad
Peter Kalverla <peter.kalverla@gmx.com>
2024-08-01 15:10:42 +0200

    Apply suggestions from code review
    Co-authored-by: Stefan Verhoeven <s.verhoeven@esciencecenter.nl>

--- apps/class-solid/src/routes/index.tsx
+++ apps/class-solid/src/routes/index.tsx
@@ -12,7 +12,6 @@ import { analyses } from "~/lib/store";
 import { addAnalysis, AnalysisCard } from "~/components/Analysis";
 
 export default function Home() {
-  console.log(import.meta.env.SERVER_BASE_URL);
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
