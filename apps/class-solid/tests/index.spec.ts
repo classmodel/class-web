45995a98956fd1c889a4e1d477701c015eca2180
Peter Kalverla <peter.kalverla@gmx.com>
2024-08-22 10:03:27 +0200

    Fix tests

--- apps/class-solid/tests/index.spec.ts
+++ apps/class-solid/tests/index.spec.ts
@@ -1,9 +1,23 @@
 import { expect, test } from "@playwright/test";
 
 test("has welcome", async ({ page }) => {
+  await page.goto("/about");
+
+  await expect(
+    page.getByRole("heading", { name: "Welcome to CLASS" })
+  ).toBeVisible();
+});
+
+test("has experiments", async ({ page }) => {
   await page.goto("/");
 
   await expect(
-    page.getByRole("heading", { name: "Welcome to CLASS" }),
+    page.getByRole("heading", { name: "Experiments" })
   ).toBeVisible();
+});
+
+test("has analysis", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page.getByRole("heading", { name: "Analysis" })).toBeVisible();
 });
