b9f42bda0401b76b02f4f66293eb5c5d1e57a911
Peter Kalverla <peter.kalverla@gmx.com>
2024-08-22 09:21:30 +0200

    Move add experiment/analysis buttons under a dropdown menu

--- apps/class-solid/src/routes/index.tsx
+++ apps/class-solid/src/routes/index.tsx
@@ -2,7 +2,15 @@ import { For, Show } from "solid-js";
 
 import { AnalysisCard, addAnalysis } from "~/components/Analysis";
 import { ExperimentCard } from "~/components/Experiment";
-import { Button } from "~/components/ui/button";
+import { MdiPlusBox } from "~/components/icons";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 import { Flex } from "~/components/ui/flex";
 
 import { addExperiment, experiments } from "~/lib/store";
@@ -11,60 +19,63 @@ import { analyses } from "~/lib/store";
 export default function Home() {
   return (
     <main class="mx-auto p-4 text-center text-gray-700">
-      <h2 class="my-8 text-4xl">Experiments</h2>
+      <h2 class="my-8 text-4xl">
+        Experiments
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <MdiPlusBox class="ml-2 inline-block align-bottom" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Add experiment</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => addExperiment()}>
+              Default settings
+            </DropdownMenuItem>
+            <DropdownMenuItem class="text-gray-400">
+              Upload (not implemented)
+            </DropdownMenuItem>
+            <DropdownMenuItem class="text-gray-400">
+              Preset (not implemented)
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </h2>
+
       <Flex justifyContent="center" class="flex-wrap gap-4">
         <For each={experiments}>
           {(experiment) => ExperimentCard(experiment)}
         </For>
-        <div>
-          <div>
-            <Button variant="outline" size="lg" onClick={() => addExperiment()}>
-              Add experiment
-            </Button>
-          </div>
-          <div>
-            <Button variant="outline" size="lg">
-              Upload experiment config (not implemented)
-            </Button>{" "}
-          </div>
-          <div>
-            <Button variant="outline" size="lg">
-              Add experiment from choice of presets (not implemented)
-            </Button>
-          </div>
-        </div>
       </Flex>
 
-      <h2 class="my-8 text-4xl">Analysis</h2>
+      <h2 class="my-8 text-4xl">
+        Analysis
+        <Show when={experiments.length}>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <MdiPlusBox class="ml-2 inline-block align-bottom" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Add analysis</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => addAnalysis()}>
+                Card
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => addAnalysis("timeseries")}>
+                Timeseries
+              </DropdownMenuItem>
+              <DropdownMenuItem class="text-gray-400">
+                Vertical profile (not implemented)
+              </DropdownMenuItem>
+              <DropdownMenuItem class="text-gray-400">
+                Skew-T diagram (not implemented)
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </Show>
+      </h2>
       <Show when={experiments.length} fallback={<p>Add an experiment first</p>}>
         <Flex justifyContent="center" class="flex-wrap gap-4">
           <For each={analyses}>{(analysis) => AnalysisCard(analysis)}</For>
-          <div>
-            <div>
-              <Button variant="outline" size="lg" onClick={() => addAnalysis()}>
-                Add standard analysis
-              </Button>
-            </div>
-            <div>
-              <Button
-                variant="outline"
-                size="lg"
-                onClick={() => addAnalysis("timeseries")}
-              >
-                Add timeseries plot
-              </Button>
-            </div>
-            <div>
-              <Button variant="outline" size="lg">
-                Add vertical profile plot (not implemented)
-              </Button>
-            </div>
-            <div>
-              <Button variant="outline" size="lg">
-                Add output table (not implemented)
-              </Button>
-            </div>
-          </div>
         </Flex>
       </Show>
     </main>
