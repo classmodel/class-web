109d18d9e6d1424e039afb4f8162741ebbca5135
Peter Kalverla <peter.kalverla@gmx.com>
2024-09-24 09:01:29 +0200

    work in progress

--- apps/class-solid/src/components/LinePlot.tsx
+++ apps/class-solid/src/components/LinePlot.tsx
@@ -1,4 +1,5 @@
 import * as d3 from "d3";
+import { createSignal } from "solid-js";
 
 export default function LinePlot({
   // x,  # TODO pass data into plot
@@ -13,38 +14,28 @@ export default function LinePlot({
   const x = [10, 10, 5, 4, 3, 2, 1]; // Dummy theta
   const y = [0, 1000, 1000, 1100, 1200, 1300, 1400]; // Dummy height
 
-  const scaleX = d3.scaleLinear(d3.extent(x), [
-    marginLeft,
-    width - marginRight,
-  ]);
-  const scaleY = d3.scaleLinear(d3.extent(y), [
-    height - marginBottom,
-    marginTop,
-  ]);
+  const scaleX = d3.scaleLinear([0, 10], [marginLeft, width - marginRight]);
+  const scaleY = d3.scaleLinear([0, 2000], [height - marginBottom, marginTop]);
 
   // Use SolidJS ref system for accessing dom elements needed for d3-axis
-  let gx!: SVGGElement;
-  let gy!: SVGGElement;
-  d3.select(gx).call(d3.axisBottom(scaleX));
-  d3.select(gy).call(d3.axisLeft(scaleY));
+  const [gx, setGx] = createSignal();
+  const [gy, setGy] = createSignal();
+  // let gx!: SVGGElement;
+  // let gy!: SVGGElement;
+  // d3.select(gx).call(d3.axisBottom(x));
+  // d3.select(gy).call(d3.axisLeft(y));
+
+  setGx(d3.svg.axis().scale(x).orient("bottom"));
+
+  console.log(gx);
 
   const l = d3.line((d, i) => scaleX(x[i]), scaleY);
 
   return (
     <svg width={width} height={height} class="border-2 border-black">
       <title>Vertical profile plot</title>
-      <g
-        ref={(el) => {
-          gx = el;
-        }}
-        transform={`translate(0,${height - marginBottom})`}
-      />
-      <g
-        ref={(el) => {
-          gy = el;
-        }}
-        transform={`translate(${marginLeft},0)`}
-      />
+      <g ref={setGx} transform={`translate(0,${height - marginBottom})`} />
+      <g ref={setGy} transform={`translate(${marginLeft},0)`} />
       <path fill="none" stroke="currentColor" strokeWidth="1.5" d={l(y)} />
       <g fill="white" stroke="currentColor" strokeWidth="1.5">
         {y.map((d, i) => (
