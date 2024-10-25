55f63516231fe1158df499dae661735edffe6024
Peter Kalverla <peter.kalverla@gmx.com>
2024-10-25 12:37:05 +0200

    Implement axes components in solidjs

--- apps/class-solid/src/components/LinePlot.tsx
+++ apps/class-solid/src/components/LinePlot.tsx
@@ -1,14 +1,15 @@
 import * as d3 from "d3";
+import { AxisBottom, AxisLeft } from "./Axes";
 
 export default function LinePlot({
   // x,  # TODO pass data into plot
   // y,
   width = 450,
   height = 400,
-  marginTop = 20,
-  marginRight = 20,
-  marginBottom = 20,
-  marginLeft = 20,
+  marginTop = 50,
+  marginRight = 50,
+  marginBottom = 50,
+  marginLeft = 50,
 }) {
   const x = [10, 10, 5, 4, 3, 2, 1]; // Dummy theta
   const y = [0, 1000, 1000, 1100, 1200, 1300, 1400]; // Dummy height
@@ -19,13 +20,17 @@ export default function LinePlot({
   const l = d3.line((d, i) => scaleX(x[i]), scaleY);
 
   return (
-    <svg width={width} height={height} class="border-2 border-black">
+    <svg width={width} height={height} class="">
       <title>Vertical profile plot</title>
-      <g transform={`translate(0,${height - marginBottom})`} />
-      <g transform={`translate(${marginLeft},0)`} />
+      <AxisBottom
+        scale={scaleX}
+        transform={`translate(0, ${height - marginBottom})`}
+      />
+      <AxisLeft scale={scaleY} transform={`translate(${marginLeft}, 0)`} />
       <path fill="none" stroke="currentColor" strokeWidth="1.5" d={l(y)} />
       <g fill="white" stroke="currentColor" strokeWidth="1.5">
         {y.map((d, i) => (
+          // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
           <circle key={i} cx={scaleX(x[i])} cy={scaleY(d)} r="2.5" />
         ))}
       </g>
