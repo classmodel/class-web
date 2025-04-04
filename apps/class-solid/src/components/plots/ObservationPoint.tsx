2f8bef09a2f4d97df258559e7cff02ec57309559
Peter Kalverla <peter.kalverla@gmx.com>
2025-04-04 13:18:43 +0200

    Display temp in kelvin so it renders on plot

--- apps/class-solid/src/components/plots/ObservationPoint.tsx
+++ apps/class-solid/src/components/plots/ObservationPoint.tsx
@@ -3,15 +3,12 @@ import { useChartContext } from "./ChartContainer";
 export function ObservationPoint(p: { x: number; y: number }) {
   const [chart, updateChart] = useChartContext();
 
-  const cx = () => chart.scaleX(p.x);
+  const cx = () => chart.scaleX(p.x + 273.15);
   const cy = () => chart.scaleY(p.y);
-
   return (
     <circle
       cx={cx()}
       cy={cy()}
-      // cx={100}
-      // cy={200}
       r={2}
       fill="red"
       stroke="black"
