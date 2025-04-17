62ecffdc92d337ce9bc66b741825a305a3d4bdc6
sverhoeven <s.verhoeven@esciencecenter.nl>
2025-04-17 14:04:55 +0200

    Revert "Wrap experiment config form in error boundary"
    This reverts commit 719a371d28e6699a2360bfeb0440db1bab06fc30.

--- apps/class-solid/src/components/ExperimentConfigForm.tsx
+++ apps/class-solid/src/components/ExperimentConfigForm.tsx
@@ -1,9 +1,8 @@
 import type { Config } from "@classmodel/class/config";
 import { Form } from "@classmodel/form";
-import { ErrorBoundary, createMemo } from "solid-js";
+import { createMemo } from "solid-js";
 import type { ExperimentConfig } from "~/lib/experiment_config";
 import { findPresetByName } from "~/lib/presets";
-import { ErrorToast } from "./ErrorToast";
 
 export function ExperimentConfigForm({
   id,
@@ -17,14 +16,12 @@ export function ExperimentConfigForm({
   const preset = createMemo(() => findPresetByName(experiment.preset));
 
   return (
-    <ErrorBoundary fallback={(error) => <ErrorToast error={error} />}>
-      <Form
-        id={id}
-        onSubmit={onSubmit}
-        values={experiment.reference}
-        defaults={preset().config}
-        schema={preset().schema}
-      />
-    </ErrorBoundary>
+    <Form
+      id={id}
+      onSubmit={onSubmit}
+      values={experiment.reference}
+      defaults={preset().config}
+      schema={preset().schema}
+    />
   );
 }
