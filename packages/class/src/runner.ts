cd10da814e7d8053ffa1eb60d18d3b6f2c6ecd7d
Peter Kalverla <peter.kalverla@gmx.com>
2025-04-18 15:21:31 +0200

    Make sure initial state is included in output

--- packages/class/src/runner.ts
+++ packages/class/src/runner.ts
@@ -89,22 +89,32 @@ export type ClassOutput = {
 
 
 export function runClass(config: Config): ClassOutput {
-  
   const validatedConfig = parse(config);
   const model = new CLASS(validatedConfig);
+
+  const writeOutput = () => {
+    for (const v of outputVariables) {
+      const value = model[v.key as keyof CLASS] ?? (v.key === "t" ? model.t : undefined);
+      if (value !== undefined) {
+        (output[v.key] as number[]).push(value as number);
+      }
+    }
+  };
+
 
   const output = Object.fromEntries(
     outputVariables.map(v => [v.key, []])
   ) as ClassOutput;
 
+  // Initial time
+  writeOutput();
+
+  // Update loop
   while (model.t < config.runtime) {
     model.update();
 
     if (model.t % 600 === 0) {
-      for (const v of outputVariables) {
-          output[v.key].push(model[v.key as keyof CLASS] as number);
-        output[v.key].push();
-      }
+      writeOutput();
     }
   }
 
